296b0275712fb8282b195e5810520af6862de334
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-04-07 22:24:35 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import axios from "axios";
+
+//create context provider, make get request to https://api.spoonacular.com/recipes/random 
+//to get random recipe, pass down via context 
+
+
+import "./App.css";
+
+function App() {
+    
+
+    return (
+        <>
+            <h1>Chicken... again?</h1>
+        </>
+    );
+}
+
+export default App;
